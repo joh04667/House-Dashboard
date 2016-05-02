403fbcb5b599b13b77c6649eaee96f5b8f33a823
Kyle Johnson <joh04667@gmail.com>
2016-05-02 13:26:14 -0500

    sets up some database endpoints

--- server/db/connection.js
+++ server/db/connection.js
@@ -8,5 +8,58 @@ if (process.env.DATABASE_URL){
   connectionString = process.env.DATABASE_URL;
 } else {
   console.log('local var');
-  connectionString = //TODO: postgres://localhost:5432/database name;
+  connectionString = "postgres://localhost:5432/blondegalow";
 }
+
+function initializeDB(){
+  pg.connect(connectionString, function(err, client,done){
+    console.log('connected to postgspwlslsql');
+    if(err){
+      console.log('Error connecting to DB!', err);
+      process.exit(1);
+    } else {
+
+        var query = client.query(
+          'CREATE TABLE IF NOT EXISTS user(' +
+          'id SERIAL PRIMARY KEY,' +
+          'username varchar(255) NOT NULL,' +
+          'password varchar(255) NOT NULL,' +
+          'display_name varchar(30) NOT NULL,' +
+          'permissions varchar(15) NOT NULL,' +
+          'active boolean NOT NULL);'
+        );
+      query.on('end', function(){
+        console.log('users table created');
+        done();
+      });
+
+      var query = client.query(
+        'CREATE TABLE IF NOT EXISTS post(' +
+        'id SERIAL PRIMARY KEY,' +
+        'message TEXT NOT NULL,' +
+        'user_id INT REFERENCES user(id));'
+      );
+      query.on('end', function(){
+        console.log('posts table created');
+        done();
+    });
+
+      var query = client.query(
+        'CREATE TABLE IF NOT EXISTS chores(' +
+        'id SERIAL PRIMARY KEY,' +
+        'task varchar(255) NOT NULL,' +
+        'date_added DATE NOT NULL,' +
+        'date_completed DATE,' +
+        'user_id INT REFERENCES user(id));'
+      );
+      query.on('end', function(){
+        console.log('chores table created');
+        done();
+      })
+
+    }
+  });
+}
+
+module.exports.connectionString = connectionString;
+module.exports.initializeDB = initializeDB;
